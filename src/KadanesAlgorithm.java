891296b3d7471a6f8008ae2f3915eafa051e87f4
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 20:36:31 -0700

    added comments for better understanding

--- src/KadanesAlgorithm.java
+++ src/KadanesAlgorithm.java
@@ -1,12 +1,17 @@
 public class KadanesAlgorithm {
 
+    // kadanes algorithm to calculate the max sum containing in a subarrary
     public static int maximumSumSubArray(int [] arr, int n){
+
+        // initially sum is zero and the comparitive value is minimum
         int sum = 0;
         int max = Integer.MIN_VALUE;
+
         for(int i=0; i<n; i++){
+            // sum is as usual and stored in max if its more than max
             sum += arr[i];
             max = Math.max(max, sum);
-
+            // but at any time if its less then 0 then we make it 0 and continue
             if(sum <0){
                 sum = 0;
             }
