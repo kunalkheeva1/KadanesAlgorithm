cd79cfb4f2965350e3c1f41600baabd95cfe805a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 20:33:55 -0700

    wrote the KadanesAlgorithm to find the maximum sum containing a subarray

--- src/KadanesAlgorithm.java
+++ src/KadanesAlgorithm.java
@@ -1,5 +1,19 @@
 public class KadanesAlgorithm {
-    
+
+    public static int maximumSumSubArray(int [] arr, int n){
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            max = Math.max(max, sum);
+
+            if(sum <0){
+                sum = 0;
+            }
+        }return max;
+    }
+
+
     public static void main(String[] args) {
 
     }
